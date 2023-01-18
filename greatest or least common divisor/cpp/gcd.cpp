c97402c99ece085ba66005104c55fe72f11c02e1
mursalatul <mursalatulislampallob@gmail.com>
2023-01-18 23:27:38 +0600

    upgrade gcd into Pgcd
    rename gcd into Pgcd so that it does not conflict with std::_gcd. check if n or m is equal to 0 or not. remove minimun and maximum checking function for more readablity.

--- greatest or least common divisor/cpp/gcd.cpp
+++ greatest or least common divisor/cpp/gcd.cpp
@@ -1,38 +1,42 @@
-#include<iostream>
-#include<algorithm> // min(), max()
+#include <iostream>
+#include <algorithm> // min(), max()
+using namespace std;
 
-long long gcd(long long n, long long m)
+long long Pgcd(long long n, long long m)
 {
-    /*finding which number is greater among n and m.
-    this step is needed cause we dont know if user
-    gave big number at first argument or last argument.*/
-    long long x = std::max(n, m);
-    long long y = std::min(n, m);
+    // 0 is dividable by any number. so other number is the gcd(0 or non zero)
+    if (n == 0)
+        return m;
+    if (m == 0)
+        return n;
     /*if max number is dividable by min then min is the gcd
     in this function all time we will find this*/
-    if(x % y == 0) return y;
-    else{
-        while(true){
-            /*replace max number with (max number % min number)
-            and check above status.
-            if true then new min number is the gcd.
-            if not then again replace present max with (max number % min number)*/
-             if(x >= y){
-                x = x % y;
-                if(y % x == 0) return x;
-             }
-             else{
-                y = y % x;
-                if(x % y == 0) return y;
-             }
+    if (max(n, m) % min(n, m) == 0)
+        return min(n, m);
+    while (true)
+    {
+        /*replace max number with (max number % min number)
+        and check above status.
+        if true then new min number is the gcd.
+        if not then again replace present max with (max number % min number)*/
+        if (n >= m)
+        {
+            n = n % m;
+            if (m % n == 0)
+                return n;
+        }
+        else
+        {
+            m = m % n;
+            if (n % m == 0)
+                return m;
         }
     }
 }
 
 int main()
 {
-    int n = 1000000000, m = 999999999;
-
-    std::cout << std::__gcd(n, m);
+    int n = 1296878609, m = 119789098;
+    cout << Pgcd(n, m);
     return 0;
 }
