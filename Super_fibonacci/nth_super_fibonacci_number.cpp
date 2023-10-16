adbe5d71bf9e5b5e55b1b9995e2689463fc7821a
Md Sajjad Islam Swadhin <91667875+S-art-stack@users.noreply.github.com>
2023-10-16 18:01:46 +0600

    Show_nth_super_fibonacci_number

---
+++ Super_fibonacci/nth_super_fibonacci_number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+long long fibonacci(long long N)
+{
+    if (N < 2)
+    {
+        return N;
+    }
+
+    else
+    {
+        return fibonacci(N - 1) + 2 * fibonacci(N - 2);
+    }
+}
+int main()
+{
+    long long p;
+    cin >> p;
+    cout << fibonacci(p) << endl;
+}
